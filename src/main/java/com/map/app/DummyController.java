f72db71e834a09b71e73e45c0c8ea9281ddbfe8f
Manishverma0000 <manishverma3224@gmail.com>
2024-09-05 14:31:06 +0530

    one dummy controller removed

--- src/main/java/com/map/app/DummyController.java
+++ src/main/java/com/map/app/DummyController.java
@@ -2,10 +2,10 @@ package com.map.app;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-public class DummyController {
-    @CrossOrigin(origins = "https://tsaas.iitr.ac.in")
-    @GetMapping("/test")
-    public String test() {
-        return "Test endpoint working!";
-    }
-}
+// public class DummyController {
+//     @CrossOrigin(origins = "https://tsaas.iitr.ac.in")
+//     @GetMapping("/test")
+//     public String test() {
+//         return "Test endpoint working!";
+//     }
+// }
